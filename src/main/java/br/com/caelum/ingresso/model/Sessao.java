c53a9aa3386eaec09925aa9d918f4811b513b56e
Leonardo Botelho <leonardocbotelho@gmail.com>
2017-12-21 20:03:52 -0200

    3.2 Preco com BigDecimal

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -1,5 +1,6 @@
 package br.com.caelum.ingresso.model;
 
+import java.math.BigDecimal;
 import java.time.LocalTime;
 
 import javax.persistence.Entity;
@@ -22,6 +23,8 @@ public class Sessao {
 	@ManyToOne
 	private Filme filme;
 
+	private BigDecimal preco;
+	
 	public Sessao() {
 		
 	}
@@ -30,6 +33,7 @@ public class Sessao {
 		this.horario = horario;
 		this.filme = filme;
 		this.sala = sala;
+		this.preco = sala.getPreco().add(filme.getPreco());
 	}
 
 	public Integer getId() {
@@ -62,6 +66,14 @@ public class Sessao {
 
 	public void setFilme(Filme filme) {
 		this.filme = filme;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
 	}
 
 }
